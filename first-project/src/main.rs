c0033190b7039ca14beac48866ee8017b57e7ee0
solawat <s.spthx.555@gmail.com>
2022-10-26 22:24:15 +0900

    第１章

--- first-project/src/main.rs
+++ first-project/src/main.rs
@@ -1,3 +1,32 @@
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+// fn main() {
+//     let added = add(10, 20);
+//     println!("{}", added);
+
+//     let z = 20;
+//     let add_z = |x: i32| x + z;
+//     println!("{}", add_z(10));
+//     println!("Hello, world!");
+// }
+
 fn main() {
-    println!("Hello, world!");
+    {
+        let x = String::from("hello");
+        let y = x;
+
+        println!("y:{}", y);
+    }
+
+    {
+        let z = String::from("hello");
+        {
+            let w = &z;
+
+            println!("w:{}", w);
+        }
+        println!("z:{}", z);
+    }
 }

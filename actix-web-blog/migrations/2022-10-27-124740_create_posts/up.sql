535c3b5031b7597604dba22aecd2e30d64fc0595
solawat <s.spthx.555@gmail.com>
2022-10-27 21:51:48 +0900

    run migrate

--- actix-web-blog/migrations/2022-10-27-124740_create_posts/up.sql
+++ actix-web-blog/migrations/2022-10-27-124740_create_posts/up.sql
@@ -1,1 +1,6 @@
--- Your SQL goes here
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR NOT NULL,
+    body TEXT NOT NULL,
+    published BOOLEAN NOT NULL DEFAULT 'f'
+);

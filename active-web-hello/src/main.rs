ddfe13c865238a8d27e7c792eed4d2c55cf64bd8
solawat <s.spthx.555@gmail.com>
2022-10-27 21:42:16 +0900

    add serde

--- active-web-hello/src/main.rs
+++ active-web-hello/src/main.rs
@@ -1,19 +1,35 @@
-use actix_web::{ // (d1)
-    http::header::ContentType, App, HttpResponse,
-    HttpServer,
-};
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize}; // (d1)
 
-#[actix_web::get("/")] // (d2)
-async fn hello() -> HttpResponse {
-    HttpResponse::Ok()
-        .append_header(ContentType::json())
-        .body(r#"{"greet":"Hello, world!"}"#)
+#[derive(Deserialize)] // (d2)
+struct HelloQuery {
+    name: String,
+    age: u32,
 }
 
-#[actix_web::main] // (d3)
+#[derive(Serialize)] // (d3)
+struct HelloResponse {
+    greet: String,
+}
+
+#[actix_web::get("/")]
+async fn hello(
+    query: web::Query<HelloQuery>,
+) -> HttpResponse {
+    let query = query.into_inner(); // (d4)
+    let message = format!(
+        "Hello, my name is {}! I am {} years old!",
+        query.name, query.age
+    );
+    let h = HelloResponse { greet: message }; // (d5)
+
+    HttpResponse::Ok().json(h) // (d6)
+}
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(hello))
-        .bind(("0.0.0.0", 8080))?
+        .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
